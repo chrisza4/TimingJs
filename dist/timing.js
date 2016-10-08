4c7e1203b504fbb51654ab4a7a1de99c96d59c9e
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2016-10-08 19:24:12 +0700

    build

--- dist/timing.js
+++ dist/timing.js
@@ -4,9 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.timing = timing;
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
 function timing(modules) {
   var time = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : console.time;
   var timeEnd = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : console.timeEnd;
@@ -18,36 +15,24 @@ function timing(modules) {
     if (typeof val === 'function') {
       (function () {
         var funcToWrapped = val;
-        newModules[key] = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+        newModules[key] = function () {
           var _arguments = arguments;
-          var argsArray,
-              result,
-              _args = arguments;
-          return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-              switch (_context.prev = _context.next) {
-                case 0:
-                  argsArray = Object.keys(_args).map(function (key) {
-                    return _arguments[key];
-                  });
 
-                  time(key + ' use');
-                  _context.next = 4;
-                  return funcToWrapped.apply(null, argsArray);
-
-                case 4:
-                  result = _context.sent;
-
-                  timeEnd(key + ' use');
-                  return _context.abrupt('return', result);
-
-                case 7:
-                case 'end':
-                  return _context.stop();
-              }
-            }
-          }, _callee, this);
-        }));
+          var argsArray = Object.keys(arguments).map(function (key) {
+            return _arguments[key];
+          });
+          time(key + ' use');
+          var funcResult = funcToWrapped.apply(null, argsArray);
+          if (!funcResult.then) {
+            timeEnd(key + ' use');
+            return funcResult;
+          } else {
+            return funcResult.then(function (result) {
+              timeEnd(key + ' use');
+              return result;
+            });
+          }
+        };
       })();
     } else {
       newModules[key] = val;
